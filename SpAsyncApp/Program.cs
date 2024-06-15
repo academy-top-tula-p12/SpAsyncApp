784a7df59f8ccc0523dbdf396c2d7e704cf71d31
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-06-15 11:47:31 +0300

    Добавьте файлы проекта.

---
+++ SpAsyncApp/Program.cs
@@ -0,0 +1,29 @@
+﻿var loop1 = LoopAsync();
+var loop2 = LoopAsync();
+var lambda = async () =>
+{
+    await Task.Run(() => Loop());
+};
+var loop3 = lambda();
+
+
+Console.WriteLine("Main work");
+
+
+await loop1;
+await loop2;
+await loop3;
+
+async Task LoopAsync()
+{
+    await Task.Run(() => Loop());
+}
+
+void Loop()
+{
+    for(int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"Task #{Task.CurrentId} - {i}");
+        Thread.Sleep(200);
+    }
+}
